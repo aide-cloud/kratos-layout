dfc81c8e4402f194d0a5082767fb364902585afb
胡标 <biao.hu@baishan.com>
2022-11-26 15:14:57 +0800

    feat: add conf.GetConfig

---
+++ internal/conf/conf.go
@@ -0,0 +1,28 @@
+package conf
+
+import (
+	"github.com/go-kratos/kratos/v2/config"
+	"github.com/go-kratos/kratos/v2/config/file"
+	"github.com/google/wire"
+)
+
+var GetConfigProviderSet = wire.NewSet(
+	GetConfig,
+	wire.FieldsOf(new(*Bootstrap), "Data"),
+)
+
+func GetConfig(flagconf string) *Bootstrap {
+	c := config.New(config.WithSource(file.NewSource(flagconf)))
+	defer c.Close()
+
+	if err := c.Load(); err != nil {
+		panic(err)
+	}
+
+	var bc Bootstrap
+	if err := c.Scan(&bc); err != nil {
+		panic(err)
+	}
+
+	return &bc
+}
